75b9861b69114fb74528e52ac72b9b862af71df0
Adriano Avelino <adriano-st777@hotmail.com>
2019-09-14 19:30:14 -0300

    updates db:seed to find_or_create_by

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cadastrando moedas..."
 
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "https://assets.chinatechnews.com/wp-content/uploads/bitcoin-logo.jpg"
-)
+coins = [
+          {
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_image: "https://assets.chinatechnews.com/wp-content/uploads/bitcoin-logo.jpg"
+          },
+          {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZCRfwkqpPvFb3QmmwGONG2i6PsgnqZ3L7dRzCNlaSTB1-ruu5"
+          },
+          {
+            description: "Dash",
+            acronym: "DASH",
+            url_image: "https://ih1.redbubble.net/image.406055498.8711/ap,550x550,12x12,1,transparent,t.png"
+          }
+        ]
 
-Coin.create!(
-    description: "Ethereum",
-    acronym: "ETH",
-    url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZCRfwkqpPvFb3QmmwGONG2i6PsgnqZ3L7dRzCNlaSTB1-ruu5"
-)
-
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://ih1.redbubble.net/image.406055498.8711/ap,550x550,12x12,1,transparent,t.png"
-)
-
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
 puts "Moedas cadastradas com sucesso!"
