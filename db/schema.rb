87354e601ba5ed65a5cd7d805f11d26d8608f110
Adriano Avelino <adriano-st777@hotmail.com>
2019-09-17 22:21:10 -0300

    updates a column from model mining_types

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_012957) do
+ActiveRecord::Schema.define(version: 2019_09_18_010300) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_17_012957) do
   end
 
   create_table "mining_types", force: :cascade do |t|
-    t.string "name"
+    t.string "description"
     t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -52,9 +52,9 @@ namespace :dev do
   desc "Cria os tipos de moedas"
   task add_mining_types: :environment do
     mining_types = [
-      {name: "Proof of Work", acronym: "PoW"},
-      {name: "Proof of Stake", acronym: "PoS"},
-      {name: "Proof of Capacity", acronym: "PoC"}
+      {description: "Proof of Work", acronym: "PoW"},
+      {description: "Proof of Stake", acronym: "PoS"},
+      {description: "Proof of Capacity", acronym: "PoC"}
     ]
 
     mining_types.each do |mining_type|
